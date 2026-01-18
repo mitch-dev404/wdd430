5d5a02772e51c64c217115aadf464305855334c1
mitch-dev404 <mitch.dev404@gmail.com>
2026-01-17 22:39:14 -0800

    working on contact list

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Contact } from '../../contact.model';
 
 @Component({
   selector: 'cms-contact-list',
@@ -6,6 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css',
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    new Contact(
+      '1',
+      'R. Kent Jackson',
+      'jacksonk@byui.edu',
+      '208-496-3771',
+      '../../assets/images/jacksonk.jpg',
+      null,
+    ),
+    new Contact(
+      '2',
+      'Rex Barzee',
+      'barzeer@byui.edu',
+      '208-496-3768',
+      '../../assets/images/barzeer.jpg',
+      null,
+    ),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
