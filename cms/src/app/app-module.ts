609bcd964293ffffda33659d877bf132cb5715c8
mitch-dev404 <mitch.dev404@gmail.com>
2026-02-21 12:40:02 -0700

    changed name of app routes file

--- cms/src/app/app-module.ts
+++ cms/src/app/app-module.ts
@@ -15,7 +15,7 @@ import { MessageItemComponent } from './messages/message-item/message-item.compo
 import { MessageEditComponent } from './messages/message-edit/message-edit.component';
 import { MessageListComponent } from './messages/message-list/message-list.component';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { AppRoutingModule } from "./appRoutes.module";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
